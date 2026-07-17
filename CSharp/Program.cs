52913bd679a3cca6a3dfcc2e774077aa720019ee
HOME <HOME@DESKTOP-IAD2HNA>
2026-07-17 18:53:02 +0200

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Programming Language!");
+        }
+    }
+}
